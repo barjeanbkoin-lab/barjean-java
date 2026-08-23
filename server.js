1d0cfe9c467e34d4c1ee9947c68d31b4dbca88f7
jeff <jeff@xemple.com>
2026-08-23 12:57:56 +0000

    final

--- server.js
+++ server.js
@@ -1,31 +1,26 @@
-app.get('/api/produits', (req, res) => {
-  res.json([
-    { nom: "T-shirt BARJEAN", prix: 5000 },
-    { nom: "Casquette", prix: 3000 }
-  ])git commit - m "ajout route produits"
-})const express = require('express');
+sconst express = require('express');
 const cors = require('cors');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('BARJEAN JAVA marche !');
+app.get("/", (req, res) => {
+  res.send("BARJEAN JAVA marche !");
 });
 
-app.get('/api/message', (req, res) => {
-  res.json({ message: "Salut depuis BARJEAN JAVA !" });
-});
-
-app.get('/api/produits', (req, res) => {
+app.get("/api/produits", (req, res) => {
   res.json([
     { nom: "T-shirt BARJEAN", prix: 5000 },
     { nom: "Casquette", prix: 3000 }
-  ])
+  ]);
+});
+
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Bienvenue chez BARJEAN" });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Serveur sur port ${PORT}`);
-})B
+  console.log("Serveur lance sur port " + PORT);
+});
