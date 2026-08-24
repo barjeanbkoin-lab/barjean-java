48c185a21ae49d604a32d9848156133028c4bd17
jeff <jeff@xemple.com>
2026-08-24 13:15:48 +0000

    Mon chat ivoire

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-const express = require("express"); const cors = require("cors"); const app = express(); app.use(cors()); app.use(express.json()); app.get("/", (req,res)=>{res.send("BARJEAN JAVA marche !");}); app.get("/api/produits",(req,res)=>{res.json([{nom:"T-shirt BARJEAN",prix:5000},{nom:"Casquette",prix:3000}]);}); const PORT = process.env.PORT||3000; app.listen(PORT,()=>console.log("Serveur sur port "+PORT));
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 10000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('Un ivoirien connecté');
+  socket.on('new_message', (msg) => {
+    io.emit('new_message', msg);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log('Chat Ivoire marche sur port ' + PORT);
+});
