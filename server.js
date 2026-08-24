c055504739fc41b429010b142ff5e2163d869d2b
jeff <jeff@xemple.com>
2026-08-24 14:07:21 +0000

    ok

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const PORT = process.env.PORT || 10000;
+const path = require('path');
+
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
-  console.log('Un ivoirien connecté');
-  socket.on('new_message', (msg) => {
-    io.emit('new_message', msg);
-  });
+    console.log('un user connecte');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
 });
 
-  http.listen(PORT, () => {
-  console.log('Chat Ivoire marche sur port ' + PORT);
-});
-
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => { console.log('Serveur lance sur ' + PORT) });
